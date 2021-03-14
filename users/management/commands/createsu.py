1c543a9b852f8699a0f2835517b9d24065929e6f
khan <k151202@naver.com>
2021-03-15 06:13:09 +0900

    Update createsu.py

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = "This command superuser"
 
     def handle(self, *args, **options):
-        admin = User.objects.get_or_none(username="ebadmin")
+        admin = User.objects.get_or_none(username="admin")
         if not admin:
             User.objects.create_superuser("admin", "k151202@naver.com", "admin")
             self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
