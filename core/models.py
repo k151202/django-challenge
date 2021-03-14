d9b6f279a73b6232c30a0a60525a412a5f0469a6
khan <k151202@naver.com>
2021-03-15 01:56:28 +0900

    copy from replit

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from . import managers
+
+class CoreModel(models.Model):
+
+  """ Core Model """
+
+  created_at = models.DateField(auto_now_add=True)
+  updated_at = models.DateField(auto_now=True)
+  objects = managers.CustomModelManager()
+
+  class Meta:
+    abstract = True
